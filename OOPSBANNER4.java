d2c5b8ce2480330fc6946fbb64cc834b122bf4c2
sindhumaram01 <sindhumaram01@gmail.com>
2026-02-27 21:24:40 +0530

    Add files via upload

---
+++ OOPSBANNER4.java
@@ -0,0 +1,40 @@
+public class OOPSBANNER4{
+
+    public static void main(String[] args) {
+
+        System.out.println(String.join(" ",
+                "  *   ", "  *   ", "  ** ", "      *** "
+        ));
+
+        System.out.println(String.join(" ",
+                "*   *", "*     *", " *   *", "  **     "
+        ));
+          System.out.println(String.join(" ",
+                "*   *", "*     *", " *   *", " **     "
+        ));
+
+        System.out.println(String.join(" ",
+                "*   *", "*     *", " *   *", "  **       "
+        ));
+
+        System.out.println(String.join(" ",
+                "*   *", "*     *", " ** ", "     ** "
+        ));
+
+        System.out.println(String.join(" ",
+                "*   *", "*     *", " *     ", "       *"
+        ));
+
+        System.out.println(String.join(" ",
+                "*   *", "*     *", " *   ", "          *"
+                
+        ));
+                System.out.println(String.join(" ",
+                "*   *", "*     *", " *   ", "        *"
+        ));
+
+        System.out.println(String.join(" ",
+                "  *   ", "  *   ", " *     ", " ** "
+        ));
+    }
+}
